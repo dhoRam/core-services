479aa99d739312ffac2498e76082c1f86c8aa23c
dhoRam <dhondiram.jadhav@lntinfotech.com>
2020-08-01 23:47:53 +0530

    test commit

--- src/main/java/com/lti/mod/services/coreservices/UserServicesApplication.java
+++ src/main/java/com/lti/mod/services/coreservices/UserServicesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class UserServicesApplication {
-
+    // registration service
 	public static void main(String[] args) {
 		SpringApplication.run(UserServicesApplication.class, args);
 	}
